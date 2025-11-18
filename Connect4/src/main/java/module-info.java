8bd03c4ea62bd0ea96b95754f569b0cdb8f17df9
Rahul Baliyan <rahulbaliyan863@gmail.com>
2025-11-18 22:40:00 +0530

    Stop tracking target and out files

--- Connect4/src/main/java/module-info.java
+++ Connect4/src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.connect4 {
+    // Required JavaFX modules
     requires javafx.controls;
     requires javafx.fxml;
 
+    // If you use any other JavaFX features, e.g., graphics:
+    requires javafx.graphics;
 
-    opens com.internshala.connectfour to javafx.fxml;
+    // Export your package containing your app to the module system
     exports com.internshala.connectfour;
+
+    // Open package for FXML access
+    opens com.internshala.connectfour to javafx.fxml;
 }
